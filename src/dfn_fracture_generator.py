d6edd6401a68d1a9d7f58c506942f5e8d378646b
Filipe Cumaru <facsa@cin.ufpe.br>
2021-04-20 12:52:39 -0300

    feat: discretization of fractures

--- src/dfn_fracture_generator.py
+++ src/dfn_fracture_generator.py
@@ -1,4 +1,39 @@
 import numpy as np
+from sortedcontainers import SortedList
+import matplotlib.pyplot as plt
+
+
+class FractureMesh(object):
+    """
+    A data structure for representation of a 1D fracture mesh.
+    """
+
+    def __init__(self, endpoints):
+        """
+        Parameters
+        ------
+        endpoints: numpy array
+            An array containing the endpoints of the line segment
+            representing the fracture.
+        """
+        self.endpoints = endpoints
+        self.nodes = SortedList(key=lambda arr: tuple(arr))
+
+    def set_mesh_nodes(self, N):
+        """
+        Set the discrete points of the mesh.
+
+        Parameters
+        ----------
+        N: int
+            Number of nodes to be generated.
+        """
+        convex_param_range = np.linspace(0, 1, N + 2).reshape((N + 2, 1))
+        nodes_coords = (1 - convex_param_range) * \
+            self.endpoints[0, :] + convex_param_range*self.endpoints[1, :]
+
+        for node_coord in nodes_coords:
+            self.nodes.add(node_coord)
 
 
 class FractureGenerator2D(object):
@@ -7,7 +42,7 @@ class FractureGenerator2D(object):
     represented by line segments.
     """
 
-    def __init__(self, num_fractures, bounding_box_dimensions):
+    def __init__(self, num_fractures, bounding_box_dimensions, num_nodes):
         """
         Parameters
         ------
@@ -15,24 +50,44 @@ class FractureGenerator2D(object):
             Number of fractures to be generated.
         bounding_box_dimensions: tuple
             Dimensions of the bounding box across the x and y axis.
+        num_nodes: int
+            Number of elements in the fracture mesh.
         """
         self.num_fractures = num_fractures
         self.bbox_dimensions = bounding_box_dimensions
         self.random_rng = np.random.default_rng()
-        self.fractures = np.zeros((self.num_fractures, 2, 2))
+        self.line_segments = np.zeros((self.num_fractures, 2, 2))
+        self.fractures_mesh = None
+        self.num_nodes = num_nodes
 
     def generate_fractures(self):
         """
         Generates the fractures and sets the fractures attribute.
         """
-        self.fractures[:, :, 0] = self.random_rng.uniform(
+        self.line_segments[:, :, 0] = self.random_rng.uniform(
             high=self.bbox_dimensions[0], low=0.0, size=(self.num_fractures, 2))
-        self.fractures[:, :, 1] = self.random_rng.uniform(
+        self.line_segments[:, :, 1] = self.random_rng.uniform(
             high=self.bbox_dimensions[1], low=0.0, size=(self.num_fractures, 2))
+        self.fractures_mesh = [FractureMesh(
+            endpoints) for endpoints in self.line_segments]
+
+        for fracture_mesh in self.fractures_mesh:
+            fracture_mesh.set_mesh_nodes(self.num_nodes)
+
+    def find_intersections(self):
+        pass
+
+    def plot_fractures(self):
+        for fracture in self.fractures_mesh:
+            nodes_coords = np.array(list(fracture.nodes.irange()))
+            plt.plot(nodes_coords[:, 0],
+                     nodes_coords[:, 1], "ro-", linewidth=3)
+        plt.show()
 
     def export_fractures_to_file(self, path):
         """
         Write the fractures matrix to a text file.
         """
-        reshaped_fractures = self.fractures.reshape((self.num_fractures, 4))
-        np.savetxt(path, reshaped_fractures)        
+        reshaped_fractures = self.line_segments.reshape(
+            (self.num_fractures, 4))
+        np.savetxt(path, reshaped_fractures)
