d28e7f95109e7551f5e135413d587839b3cb5f0c
Filipe Cumaru <facsa@cin.ufpe.br>
2021-05-05 11:47:52 -0300

    feat: partial implementation of the sweep line algorithm

--- src/dfn_fracture_generator.py
+++ src/dfn_fracture_generator.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sortedcontainers import SortedList
 import matplotlib.pyplot as plt
-from .utils import segments_intersect
+from .utils import segments_intersect, LineSweepStatusTree, LineSegmentTreeNode
 
 
 class Fracture(object):
@@ -90,26 +90,62 @@ class FractureGenerator2D(object):
         Check fractures for intersections and add points to the
         fracture mesh.
         """
-        # TODO: Usar estratégia line sweep para reduzir
-        # o número de interseções.
+        flattened_endpoints = self.line_segments.reshape(
+            (self.num_fractures * 2, 2))
+        event_queue = SortedList(key=lambda event_point: tuple(event_point))
+        # Segments by event point (L for lower endpoint, U for upper, and C for containing).
+        L, U, C = {}, {}, {}
+        for fracture in self.fractures:
+            upper_endpoint, lower_endpoint = fracture.endpoints[0], fracture.endpoints[1]
 
-        for f1 in self.fractures:
-            for f2 in self.fractures:
-                if f1.id != f2.id and segments_intersect(f1.endpoints, f2.endpoints):
-                    # Vector directions for the segments.
-                    u = f1.endpoints[1] - f1.endpoints[0]
-                    v = f2.endpoints[1] - f2.endpoints[0]
-                    w = f2.endpoints[0] - f1.endpoints[0]
+            event_queue.add(upper_endpoint)
+            if tuple(upper_endpoint) in U:
+                U[tuple(upper_endpoint)] = [fracture.id]
+            else:
+                U[tuple(upper_endpoint)].append(fracture.id)
 
-                    # Parameters of the intersection.
-                    s = np.cross(w, v) / np.cross(v, u)
+            event_queue.add(lower_endpoint)
+            if tuple(lower_endpoint) in U:
+                L[tuple(lower_endpoint)] = [fracture.id]
+            else:
+                L[tuple(lower_endpoint)].append(fracture.id)
 
-                    # Intersection point.
-                    I = f1.endpoints[0] - s*u
+            C[tuple(upper_endpoint)] = []
+            C[tuple(lower_endpoint)] = []
 
-                    # Insert point into fracture's mesh.
-                    f1.nodes.add(I)
-                    f2.nodes.add(I)
+        status = LineSweepStatusTree()
+        intersections = []
+
+        while len(event_queue) > 0:
+            p = event_queue.pop(-1)
+            L_p, U_p, C_p = L[tuple(p)], U[tuple(p)], C[tuple(p)]
+            all_containing_p = set(L_p) | set(U_p) | set(C_p)
+
+            # If point is in more than one segment, then it is an intersection.
+            if len(all_containing_p) > 1:
+                intersections.append((p, all_containing_p))
+
+            # We then remove all segments from the status tree that have
+            # p as a lower endpoint or inside it.
+            segments_to_remove = set(L_p) | set(C_p)
+            for line in segments_to_remove:
+                node = status.find(LineSegmentTreeNode(
+                    line, self.fractures[line].endpoints))
+                if node is not None:
+                    status.remove(node)
+
+            lines_to_reinsert = set(U_p) | set(C_p)
+            for line in lines_to_reinsert:
+                status.insert(LineSegmentTreeNode(
+                    line, self.fractures[line].endpoints))
+
+            if len(lines_to_reinsert) == 0:
+                pass
+            else:
+                pass
+
+    def _find_new_event_point(self):
+        pass
 
     def plot_fractures(self):
         for fracture in self.fractures:
